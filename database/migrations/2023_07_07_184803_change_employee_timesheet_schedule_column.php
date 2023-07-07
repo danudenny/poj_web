07d1a1e5f19c5f675d872170c36aef27a77f25af
Denny Danuwijaya <danudenny@gmail.com>
2023-07-08 01:55:10 +0700

    [TIMESHEET] leaftover migration

--- database/migrations/2023_07_07_184803_change_employee_timesheet_schedule_column.php
+++ database/migrations/2023_07_07_184803_change_employee_timesheet_schedule_column.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,10 +14,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('periods', function (Blueprint $table) {
-            $table->bigInteger('year')->change();
-            $table->bigInteger('month')->change();
-        });
+        DB::statement('ALTER TABLE periods ALTER COLUMN year TYPE bigint USING year::bigint');
+        DB::statement('ALTER TABLE periods ALTER COLUMN month TYPE bigint USING month::bigint');
+
     }
 
     /**
