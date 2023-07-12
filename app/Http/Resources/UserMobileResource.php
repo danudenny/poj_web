f1cbae30368f8e457b4710880df7c64dc269d6bd
Denny Danuwijaya <danudenny@gmail.com>
2023-07-13 05:47:41 +0700

    checkout logic

--- app/Http/Resources/UserMobileResource.php
+++ app/Http/Resources/UserMobileResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserMobileResource extends JsonResource
@@ -9,10 +11,10 @@ class UserMobileResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
         return [
             'id' => $this->id,
@@ -30,6 +32,11 @@ class UserMobileResource extends JsonResource
             'is_backup_checkin' => $this->is_backup_checkin,
             'is_overtime_checkin' => $this->is_overtime_checkin,
             'is_longshift_checkin' => $this->is_longshift_checkin,
+            'is_normal_checkout' => $this->is_normal_checkout,
+            'is_event_checkout' => $this->is_event_checkout,
+            'is_backup_checkout' => $this->is_backup_checkout,
+            'is_overtime_checkout' => $this->is_overtime_checkout,
+            'is_longshift_checkout' => $this->is_longshift_checkout,
             'roles' => RoleResource::collection($this->whenLoaded('roles')),
         ];
     }
