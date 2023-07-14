7aae2f6341a3b039e280af323865f6fdbabfa687
Denny Danuwijaya <danudenny@gmail.com>
2023-07-14 21:40:44 +0700

    fix migration

--- database/migrations/2023_07_14_090113_alter_events.php
+++ database/migrations/2023_07_14_090113_alter_events.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('events', function(Blueprint $table) {
-            $table->enum('location_type', ['internal', 'external']);
+            $table->enum('location_type', ['internal', 'external'])->default('internal');
         });
     }
 
