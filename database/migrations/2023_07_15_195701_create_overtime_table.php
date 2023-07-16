de2d647911734a8da17b32fc2d113a12022f95dd
freekup <fjrids@gmail.com>
2023-07-16 15:44:04 +0700

    Add migration file

---
+++ database/migrations/2023_07_15_195701_create_overtime_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('overtimes', function (Blueprint $table) {
+            $table->id();
+            $table->integer('requestor_employee_id');
+            $table->timestamp('approved_at')->nullable();
+            $table->date('date_overtime');
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->text('notes');
+            $table->text('image_url')->nullable();
+            $table->float('location_lat');
+            $table->float('location_long');
+            $table->timestamps();
+        });
+
+        Schema::create('overtime_histories', function (Blueprint $table) {
+            $table->id();
+            $table->integer('overtime_id');
+            $table->integer('employee_id');
+            $table->enum('history_type', ['submitted', 'check-in', 'check-out', 'approved']);
+            $table->timestamps();
+        });
+
+        Schema::create('overtime_employees', function (Blueprint $table) {
+            $table->id();
+            $table->integer('employee_id');
+            $table->integer('overtime_id');
+            $table->timestamp('check_in_time');
+            $table->float('check_in_lat');
+            $table->float('check_in_long');
+            $table->timestamp('check_out_time');
+            $table->float('check_out_lat');
+            $table->float('check_out_long');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('overtimes');
+        Schema::dropIfExists('overtime_histories');
+        Schema::dropIfExists('overtime_employees');
+    }
+};
