b1cb3915f0d42390478e40b5886e336e2529e60f
freekup <fjrids@gmail.com>
2023-07-19 16:28:00 +0700

    Change notification

--- database/migrations/2023_07_19_084741_create_table_notification.php
+++ database/migrations/2023_07_19_084741_create_table_notification.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('notifications', function (Blueprint $table) {
+        Schema::create('employee_notifications', function (Blueprint $table) {
             $table->id();
             $table->integer('employee_id');
             $table->string('title');
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('notifications');
+        Schema::dropIfExists('employee_notifications');
     }
 };
