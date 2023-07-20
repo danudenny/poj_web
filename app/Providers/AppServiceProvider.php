97468565abd11ba9e42151653e97fde96c872793
freekup <fjrids@gmail.com>
2023-07-20 23:40:07 +0700

    Enable DB configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,46 +32,46 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         ini_set('max_execution_time', 300);
-//        $getSetting = DB::table('settings')->get();
-//
-//        foreach ($getSetting as $value) {
-//            if ($value->key == 'MINIO_KEY') {
-//                config(['filesystems.disks.s3.key' => $value->value]);
-//            }
-//            if ($value->key == 'MINIO_SECRET') {
-//                config(['filesystems.disks.s3.secret' => $value->value]);
-//            }
-//            if ($value->key == 'MINIO_ENDPOINT') {
-//                config(['filesystems.disks.s3.endpoint' => $value->value]);
-//            }
-//            if ($value->key == 'MINIO_BUCKET') {
-//                config(['filesystems.disks.s3.bucket' => $value->value]);
-//            }
-//            if ($value->key == 'MINIO_URL') {
-//                config(['filesystems.disks.s3.url' => $value->value]);
-//            }
-//            if ($value->key == 'app_name') {
-//                config(['app.name' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_TYPE') {
-//                config(['mail.mailers.smtp.transport' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_HOST') {
-//                config(['mail.mailers.smtp.host' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_USERNAME') {
-//                config(['mail.mailers.smtp.username' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_PASSWORD') {
-//                config(['mail.mailers.smtp.password' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_ENCRYPTION') {
-//                config(['mail.mailers.smtp.encryption' => $value->value]);
-//            }
-//            if ($value->key == 'MAIL_PORT') {
-//                config(['mail.mailers.smtp.port' => $value->value]);
-//            }
-//        }
+        $getSetting = DB::table('settings')->get();
+
+        foreach ($getSetting as $value) {
+            if ($value->key == 'MINIO_KEY') {
+                config(['filesystems.disks.s3.key' => $value->value]);
+            }
+            if ($value->key == 'MINIO_SECRET') {
+                config(['filesystems.disks.s3.secret' => $value->value]);
+            }
+            if ($value->key == 'MINIO_ENDPOINT') {
+                config(['filesystems.disks.s3.endpoint' => $value->value]);
+            }
+            if ($value->key == 'MINIO_BUCKET') {
+                config(['filesystems.disks.s3.bucket' => $value->value]);
+            }
+            if ($value->key == 'MINIO_URL') {
+                config(['filesystems.disks.s3.url' => $value->value]);
+            }
+            if ($value->key == 'app_name') {
+                config(['app.name' => $value->value]);
+            }
+            if ($value->key == 'MAIL_TYPE') {
+                config(['mail.mailers.smtp.transport' => $value->value]);
+            }
+            if ($value->key == 'MAIL_HOST') {
+                config(['mail.mailers.smtp.host' => $value->value]);
+            }
+            if ($value->key == 'MAIL_USERNAME') {
+                config(['mail.mailers.smtp.username' => $value->value]);
+            }
+            if ($value->key == 'MAIL_PASSWORD') {
+                config(['mail.mailers.smtp.password' => $value->value]);
+            }
+            if ($value->key == 'MAIL_ENCRYPTION') {
+                config(['mail.mailers.smtp.encryption' => $value->value]);
+            }
+            if ($value->key == 'MAIL_PORT') {
+                config(['mail.mailers.smtp.port' => $value->value]);
+            }
+        }
 
     }
 }
