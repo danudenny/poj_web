71722cf1cd131da388e95813d86ee0f7f707b43f
Denny Danuwijaya <danudenny@gmail.com>
2023-08-14 13:19:36 +0700

    fix if no auth redirect to

--- resources/js/src/router/index.js
+++ resources/js/src/router/index.js
@@ -1020,19 +1020,18 @@ const permissions = JSON.parse(localStorage.getItem('USER_PERMISSIONS'));
 router.beforeEach((to, from, next) => {
     if (to.matched.some(record => record.meta.requiresAuth)) {
 
-        // console.log(!permissions && !permissions.includes(to.meta.permission))
-      // if (!store.getters.isAuthenticated) {
-      //   next('/auth/login');
-      // } else if (!permissions || !permissions.includes(to.meta.permission)) {
-      //     useToast().error('You are not authorized to access this page', {
-      //           duration: 5000,
-      //           position: 'top-center',
-      //           isClosable: true
-      //     });
-      //     next('/');
-      // } else {
-      //   next();
-      // }
+      if (!store.getters.isAuthenticated) {
+        next('/auth/login');
+      } else if (!permissions || !permissions.includes(to.meta.permission)) {
+          useToast().error('You are not authorized to access this page', {
+                duration: 5000,
+                position: 'top-center',
+                isClosable: true
+          });
+          next('/');
+      } else {
+        next();
+      }
 
         next();
     } else if (!to.meta.requiresAuth && store.getters.isAuthenticated) {
