7a5ac2b77e00b1145e878e00d04a7e38d7e04eb1
freekup <fjrids@gmail.com>
2023-08-24 18:10:55 +0700

    Add priority

---
+++ database/migrations/2023_08_24_111013_alter_table_attendance_approval.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('attendance_approvals', function(Blueprint $table) {
+            if (!Schema::hasColumn('attendance_approvals', 'priority')) {
+                $table->integer('priority');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
