87a61ffe881cf6763a16ffaaf5f98750eefe7f3b
freekup <fjrids@gmail.com>
2023-10-09 10:21:42 +0700

    remove check in validation

--- app/Services/Core/OvertimeService.php
+++ app/Services/Core/OvertimeService.php
@@ -826,12 +826,6 @@ class OvertimeService extends ScheduleService
 
             $notes = $request->input('notes');
             $checkInAttachmentURL = $request->input('attachment_url');
-            if ($approvalType === AttendanceApproval::TypeOffsite && (!$notes && !$checkInAttachmentURL)) {
-                return response()->json([
-                    'status' => false,
-                    'message' => 'Check In Diluar Jangkauan, Mohon Memasukkan Catatan'
-                ], ResponseAlias::HTTP_BAD_REQUEST);
-            }
 
             DB::beginTransaction();
 
